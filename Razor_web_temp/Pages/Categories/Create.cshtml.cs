ebdbe0b008ef08e76342ebf006b09d488a92ca23
Likhitha2503 <sunkaralikhitha25@gmail.com>
2023-09-06 15:06:43 +0530

    Add project files.

---
+++ Razor_web_temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor_web_temp.Data;
+using Razor_web_temp.Models;
+
+namespace Razor_web_temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
